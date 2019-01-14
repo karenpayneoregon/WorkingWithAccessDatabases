9a00f58c7c218d0fc44af3691cb29352b8664dbe
Karen Payne <paynekaren@comcast.net>
2019-01-14 05:42:22 -0800

    Added helper class for OleDb

---
+++ KarensBaseClasses/DataHelpers/OleDbHelpers.vb
@@ -0,0 +1,21 @@
+﻿Public Class OleDbHelpers
+    ''' <summary>
+    ''' Determine if OleDb data provider is installed from 2007 + products such as
+    ''' MS-Access, MS-Excel
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function IsOleDbProviderInstalled() As Boolean
+        Dim providerInstalled = (New OleDb.OleDbEnumerator()).
+                GetElements().AsEnumerable().
+                FirstOrDefault(
+                    Function(row)
+                        Return row.Field(Of String)("SOURCES_NAME") = "Microsoft.ACE.OLEDB.12.0"
+                    End Function)
+
+        If providerInstalled IsNot Nothing Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
