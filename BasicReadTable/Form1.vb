fd521911928c44a05fa589bbf9dbd97c5e8c6ee5
Karen Payne <paynekaren@comcast.net>
2019-01-21 04:55:58 -0800

    Added basic read project

---
+++ BasicReadTable/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private bsCustomers As New BindingSource
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) _
+    Handles Me.Shown
+        'DataGridView1.AutoGenerateColumns = False
+
+        Dim ops As New DatabaseOperations
+
+        If ops.IsSuccessFul Then
+            bsCustomers.DataSource = ops.LoadCustomers
+            DataGridView1.DataSource = bsCustomers
+        Else
+            MessageBox.Show(ops.LastExceptionMessage)
+        End If
+
+    End Sub
+End Class
---
+++ BasicReadTable/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("BasicReadTable")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("BasicReadTable")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c5e56f53-fca0-42f9-b4ee-0595fa964e6c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
