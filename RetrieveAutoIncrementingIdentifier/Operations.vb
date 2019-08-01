4c43fcf198c115ba4190cd8ce17df67b792c04eb
Karen Payne <paynekaren@comcast.net>
2019-08-01 03:17:01 -0700

    Clean up

--- RetrieveAutoIncrementingIdentifier/Operations.vb
+++ RetrieveAutoIncrementingIdentifier/Operations.vb
@@ -1,24 +1,11 @@
 ﻿Imports System.Data.OleDb
 
 Public Class Operations
-    Private mHasException As Boolean = False
-    Public ReadOnly Property IsSuccessful() As Boolean
-        Get
-            Return mHasException = False
-        End Get
-    End Property
-    Private mException As Exception
-    Public ReadOnly Property LastException() As Exception
-        Get
-            Return mException
-        End Get
-    End Property
     Public ReadOnly Property ConnectionString As String
         Get
             Return "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database1.accdb"
         End Get
     End Property
-    'SELECT TOP 1 Admin.ID FROM Admin ORDER BY Admin.ID DESC;
     Public Function CurrentIdentifier() As Integer
         Using cn As New OleDbConnection(ConnectionString)
             Using cmd As New OleDbCommand With
