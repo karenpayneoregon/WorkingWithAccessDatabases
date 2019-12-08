eeb050ebfaadf4efa213d1b7232051a617e6edad
Karen Payne <paynekaren@comcast.net>
2019-12-08 08:23:01 -0800

    Added code to existing project to answer a forum question

--- JustLetMeCodeAlready/Form1.Designer.vb
+++ JustLetMeCodeAlready/Form1.Designer.vb
@@ -29,26 +29,36 @@ Partial Class Form1
         Me.firstNameFindBadTextBox = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.StartDateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.EndDateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.TitleTextBox = New System.Windows.Forms.TextBox()
+        Me.LoadButton = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
+        Me.Panel1.Controls.Add(Me.GroupBox1)
         Me.Panel1.Controls.Add(Me.closeButton)
         Me.Panel1.Controls.Add(Me.employeeLastNameFindTextBox)
         Me.Panel1.Controls.Add(Me.Button2)
         Me.Panel1.Controls.Add(Me.firstNameFindBadTextBox)
         Me.Panel1.Controls.Add(Me.Button1)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel1.Location = New System.Drawing.Point(0, 264)
+        Me.Panel1.Location = New System.Drawing.Point(0, 249)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(800, 53)
+        Me.Panel1.Size = New System.Drawing.Size(800, 139)
         Me.Panel1.TabIndex = 0
         '
         'closeButton
         '
-        Me.closeButton.Location = New System.Drawing.Point(713, 12)
+        Me.closeButton.Location = New System.Drawing.Point(713, 93)
         Me.closeButton.Name = "closeButton"
         Me.closeButton.Size = New System.Drawing.Size(75, 23)
         Me.closeButton.TabIndex = 4
@@ -95,22 +105,101 @@ Partial Class Form1
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(800, 264)
+        Me.DataGridView1.Size = New System.Drawing.Size(800, 249)
         Me.DataGridView1.TabIndex = 1
+        '
+        'StartDateTimePicker
+        '
+        Me.StartDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.StartDateTimePicker.Location = New System.Drawing.Point(12, 37)
+        Me.StartDateTimePicker.Name = "StartDateTimePicker"
+        Me.StartDateTimePicker.Size = New System.Drawing.Size(136, 20)
+        Me.StartDateTimePicker.TabIndex = 5
+        Me.StartDateTimePicker.Value = New Date(1992, 4, 1, 0, 0, 0, 0)
+        '
+        'EndDateTimePicker
+        '
+        Me.EndDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.EndDateTimePicker.Location = New System.Drawing.Point(159, 37)
+        Me.EndDateTimePicker.Name = "EndDateTimePicker"
+        Me.EndDateTimePicker.Size = New System.Drawing.Size(136, 20)
+        Me.EndDateTimePicker.TabIndex = 6
+        Me.EndDateTimePicker.Value = New Date(1992, 8, 14, 0, 0, 0, 0)
+        '
+        'TitleTextBox
+        '
+        Me.TitleTextBox.Location = New System.Drawing.Point(301, 37)
+        Me.TitleTextBox.Name = "TitleTextBox"
+        Me.TitleTextBox.Size = New System.Drawing.Size(130, 20)
+        Me.TitleTextBox.TabIndex = 7
+        Me.TitleTextBox.Text = "Sales Representative"
+        '
+        'LoadButton
+        '
+        Me.LoadButton.Location = New System.Drawing.Point(437, 34)
+        Me.LoadButton.Name = "LoadButton"
+        Me.LoadButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoadButton.TabIndex = 8
+        Me.LoadButton.Text = "Load"
+        Me.LoadButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.LoadButton)
+        Me.GroupBox1.Controls.Add(Me.TitleTextBox)
+        Me.GroupBox1.Controls.Add(Me.EndDateTimePicker)
+        Me.GroupBox1.Controls.Add(Me.StartDateTimePicker)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 55)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(530, 72)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "HireDate >="
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(156, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "HireDate <="
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(303, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Title ="
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 317)
+        Me.ClientSize = New System.Drawing.Size(800, 388)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
+        Me.Text = "Just let me code (but not cool to write data operations in the form)"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -122,4 +211,12 @@ Partial Class Form1
     Friend WithEvents employeeLastNameFindTextBox As TextBox
     Friend WithEvents Button2 As Button
     Friend WithEvents closeButton As Button
+    Friend WithEvents StartDateTimePicker As DateTimePicker
+    Friend WithEvents EndDateTimePicker As DateTimePicker
+    Friend WithEvents TitleTextBox As TextBox
+    Friend WithEvents LoadButton As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
--- JustLetMeCodeAlready/Form1.vb
+++ JustLetMeCodeAlready/Form1.vb
@@ -1,15 +1,53 @@
 ﻿Imports System.Data.OleDb
-Imports System.IO
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Dotnet_Development\VS2017\MSDN_Access1\JustLetMeCodeAlready\bin\Debug\Database1.accdb"
+        Dim connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Dotnet_Development\VS2017\MSDN_Access1\JustLetMeCodeAlready\bin\Debug\Database1.accdb"
 
         Using cn As New OleDbConnection(connectionString)
             Using cmd As New OleDbCommand With {.Connection = cn}
                 cmd.CommandText = "SELECT Employees.EmployeeID, Employees.FirstName, Employees.LastName, Employees.Title, Employees.HireDate FROM Employees;"
                 Dim dt As New DataTable With {.TableName = "Employees"}
+                Try
+                    cn.Open()
+                    Dim ds As New DataSet
+                    Dim employeeTable As New DataTable With {.TableName = "Employees"}
+                    ds.Tables.Add(employeeTable)
+                    ds.Load(cmd.ExecuteReader(), LoadOption.OverwriteChanges, employeeTable)
+                    DataGridView1.DataSource = ds.Tables("Employees")
+                    DataGridView1.Columns("EmployeeID").Visible = False
+                Catch ex As Exception
+                    ' very common for a developer to simply ignore errors, unwise.
+                End Try
+            End Using
+        End Using
+    End Sub
+    ''' <summary>
+    ''' This code is not mentioned in the TechNet article. This was to answer a
+    ''' question in the Microsoft developer's forum where the person asking a question
+    ''' about a WHERE condition not working. They were using string concatenation for 
+    ''' dates and were attempting to format the DateTimePicker.Value which will cause
+    ''' a query to return unexpected results or throw an exception.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub LoadButton_Click(sender As Object, e As EventArgs) Handles LoadButton.Click
+        Dim connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" &
+                               "Data Source=Database1.accdb"
+
+        Using cn As New OleDbConnection(connectionString)
+            Using cmd As New OleDbCommand With {.Connection = cn}
+                cmd.CommandText = "SELECT EmployeeID,FirstName,LastName,Title,HireDate " &
+                                  "FROM Employees " &
+                                  "WHERE HireDate >= @StartDate AND HireDate <= @EndDate AND Title = @Title"
+
+                cmd.Parameters.AddWithValue("@StartDate", StartDateTimePicker.Value)
+                cmd.Parameters.AddWithValue("@EndDate", EndDateTimePicker.Value)
+                cmd.Parameters.AddWithValue("@Title", TitleTextBox.Text)
+
+                Dim dt As New DataTable With {.TableName = "Employees"}
+
                 Try
                     cn.Open()
                     Dim ds As New DataSet
@@ -75,4 +113,6 @@ Public Class Form1
     Private Sub closeButton_Click(sender As Object, e As EventArgs) Handles closeButton.Click
         Close()
     End Sub
+
+
 End Class
