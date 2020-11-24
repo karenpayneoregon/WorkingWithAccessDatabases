c7d1bddd813564e36182a07ec610eba3d3e63531
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-24 12:44:01 -0800

    Added form to TableAdapter project for forum question

---
+++ TableAdapter1/Form2.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Database1DataSet = New TableAdapter1.Database1DataSet()
+        Me.CustomersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CustomersTableAdapter = New TableAdapter1.Database1DataSetTableAdapters.CustomersTableAdapter()
+        Me.TableAdapterManager = New TableAdapter1.Database1DataSetTableAdapters.TableAdapterManager()
+        Me.IterateButton = New System.Windows.Forms.Button()
+        CType(Me.Database1DataSet,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.CustomersBindingSource,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'Database1DataSet
+        '
+        Me.Database1DataSet.DataSetName = "Database1DataSet"
+        Me.Database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'CustomersBindingSource
+        '
+        Me.CustomersBindingSource.DataMember = "Customers"
+        Me.CustomersBindingSource.DataSource = Me.Database1DataSet
+        '
+        'CustomersTableAdapter
+        '
+        Me.CustomersTableAdapter.ClearBeforeFill = true
+        '
+        'TableAdapterManager
+        '
+        Me.TableAdapterManager.BackupDataSetBeforeUpdate = false
+        Me.TableAdapterManager.CustomersContactTitleTableAdapter = Nothing
+        Me.TableAdapterManager.CustomersTableAdapter = Me.CustomersTableAdapter
+        Me.TableAdapterManager.EmployeesTableAdapter = Nothing
+        Me.TableAdapterManager.OrdersTableAdapter = Nothing
+        Me.TableAdapterManager.SuppliersContactTitleTableAdapter = Nothing
+        Me.TableAdapterManager.SuppliersTableAdapter = Nothing
+        Me.TableAdapterManager.UpdateOrder = TableAdapter1.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete
+        '
+        'IterateButton
+        '
+        Me.IterateButton.Location = New System.Drawing.Point(128, 57)
+        Me.IterateButton.Name = "IterateButton"
+        Me.IterateButton.Size = New System.Drawing.Size(75, 23)
+        Me.IterateButton.TabIndex = 3
+        Me.IterateButton.Text = "Iterate"
+        Me.IterateButton.UseVisualStyleBackColor = true
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(361, 132)
+        Me.Controls.Add(Me.IterateButton)
+        Me.Name = "Form2"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form2"
+        CType(Me.Database1DataSet,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.CustomersBindingSource,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+
+End Sub
+
+    Friend WithEvents Database1DataSet As Database1DataSet
+    Friend WithEvents CustomersBindingSource As BindingSource
+    Friend WithEvents CustomersTableAdapter As Database1DataSetTableAdapters.CustomersTableAdapter
+    Friend WithEvents TableAdapterManager As Database1DataSetTableAdapters.TableAdapterManager
+    Friend WithEvents IterateButton As Button
+End Class
---
+++ TableAdapter1/Form2.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form2
+    Private Sub IterateButton_Click(sender As Object, e As EventArgs) Handles IterateButton.Click
+
+        Dim customersDataTable As Database1DataSet.CustomersDataTable = Database1DataSet.Customers
+        Dim rows As DataRowCollection = customersDataTable.Rows
+
+        For Each row as DataRow In rows.Cast(of DataRow)
+            Console.WriteLine($"ID: {row.Field(of Integer)("Identifier")} Name: {row.Field(of string)("CompanyName")}")
+        Next
+
+    End Sub
+End Class
